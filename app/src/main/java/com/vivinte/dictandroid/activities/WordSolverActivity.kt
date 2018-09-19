d0910395e9c288b4750f965518f97a7b00cd9117
HadiAsiaie <h.asiayfard@gmail.com>
2018-09-20 02:26:47 +0430

    save

---
+++ app/src/main/java/com/vivinte/dictandroid/activities/WordSolverActivity.kt
@@ -0,0 +1,13 @@
+package com.vivinte.dictandroid.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.vivinte.dictandroid.R
+
+class WordSolverActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_solver)
+    }
+}
